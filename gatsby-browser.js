16d97ac8159e95f3580c5af5c89ba0e0e660f3e9
Ilya Lyamkin <ilya.lyamkin@gmail.com>
2020-05-21 11:15:43 +0200

    feat: respect correct pixel ratio

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
 import "./src/styles/global.css"
-require("prismjs/themes/prism.css")
-require("prismjs/plugins/line-highlight/prism-line-highlight.css")
+import "prismjs/themes/prism.css"
+import "prismjs/plugins/line-highlight/prism-line-highlight.css"
